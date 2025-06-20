ee0dde198d8a3c6349b2b136a3932a9227a9c47f
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-06-20 20:43:21 +0300

    add graph visualization with gizmos

---
+++ Assets/Scripts/Graph/GraphVisualization.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class GraphVisualization : MonoBehaviour
+{
+    public Graph Graph;
+    public int HierarchyLevel;
+
+    [Header("Gizmos settings")]
+    public float radius;
+    public float radiusCube;
+    
+    private void OnDrawGizmos()
+    {
+        if (Graph == null) return;
+        foreach (KeyValuePair<int3,Node> pair in Graph.NodesMap)
+        {
+            if (pair.Value.hierrarchyLevel < HierarchyLevel) continue;
+            
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(new Vector3(pair.Value.pos.x, pair.Value.pos.y, pair.Value.pos.z), radius);
+            foreach (Edge edge in pair.Value.outgoingEdges)
+            {
+                if (edge.hierrarchyLevel < HierarchyLevel) continue;
+                Gizmos.color = Color.red;
+                var from = new Vector3(pair.Value.pos.x, pair.Value.pos.y, pair.Value.pos.z);
+                var to = new Vector3(edge.destinationNode.pos.x, edge.destinationNode.pos.y, edge.destinationNode.pos.z);
+                Gizmos.DrawLine(from, to);
+            }
+            
+        }
+    }
+}
